f545a941988eb9278e9714f3add46c4d3e91b19d
technicalwajahat <wajahat.awan.924@gmail.com>
2024-12-19 18:33:54 -0500

    modified code

--- load_model.py
+++ load_model.py
@@ -3,5 +3,5 @@ from tensorflow.keras.models import load_model
 # Load the .h5 model file
 model = load_model('best_model_-10.h5')
 
-# Print the summary of the model
+# Print the Summary of the Model
 print(model.summary())
