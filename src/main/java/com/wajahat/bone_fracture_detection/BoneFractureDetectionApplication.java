816d7643bb5e622a356c5a7042608a8203420b5c
technicalwajahat <wajahat.awan.924@gmail.com>
2025-01-01 19:31:12 +0500

    spring boot application

---
+++ src/main/java/com/wajahat/bone_fracture_detection/BoneFractureDetectionApplication.java
@@ -0,0 +1,12 @@
+package com.wajahat.bone_fracture_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoneFractureDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoneFractureDetectionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wajahat/bone_fracture_detection/BoneFractureDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wajahat.bone_fracture_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoneFractureDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
