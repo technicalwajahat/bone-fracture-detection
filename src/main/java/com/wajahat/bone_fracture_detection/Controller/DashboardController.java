aa415fc4ea5864a74e4116740f039bfc5462a5d4
technicalwajahat <wajahat.awan.924@gmail.com>
2025-01-06 18:37:16 +0500

    create html pages

--- src/main/java/com/wajahat/bone_fracture_detection/Controller/DashboardController.java
+++ src/main/java/com/wajahat/bone_fracture_detection/Controller/DashboardController.java
@@ -1,1 +1,48 @@
 package com.wajahat.bone_fracture_detection.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class DashboardController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/doctor/feedback")
+    public String viewFeedback() {
+        return "view_feedback";
+    }
+
+    @GetMapping("/patient/feedback/add")
+    public String addFeedback() {
+        return "add_feedback";
+    }
+
+    @GetMapping("/patient/doctor")
+    public String viewDoctor() {
+        return "view_doctors";
+    }
+
+    @GetMapping("/doctor/patient")
+    public String viewPatient() {
+        return "view_patient";
+    }
+
+    @GetMapping("/patient/appointment/add")
+    public String addAppointment() {
+        return "add_appointment";
+    }
+
+    @GetMapping("/patient/appointment/edit")
+    public String editAppointment() {
+        return "edit_appointment";
+    }
+
+    @GetMapping("/patient/appointment")
+    public String appointment() {
+        return "edit_appointment";
+    }
+}
