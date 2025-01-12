c81de2b0105b45edc4a335375a165b95b2cde6dc
technicalwajahat <wajahat.awan.924@gmail.com>
2025-01-12 12:53:26 +0500

    modified login page

--- src/main/java/com/wajahat/bone_fracture_detection/Controller/DashboardController.java
+++ src/main/java/com/wajahat/bone_fracture_detection/Controller/DashboardController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DashboardController {
 
-   
-
     @GetMapping("/")
     public String index() {
         return "index";
