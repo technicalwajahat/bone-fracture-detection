bdfb8d46743cd5963f15436bafa1995bb3a0251f
technicalwajahat <wajahat.awan.924@gmail.com>
2025-01-13 21:52:50 +0500

    add controllers

--- src/main/java/com/wajahat/bone_fracture_detection/Controller/DashboardController.java
+++ src/main/java/com/wajahat/bone_fracture_detection/Controller/DashboardController.java
@@ -6,43 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DashboardController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
+    @GetMapping("/doctor/dashboard")
+    public String doctorDashboard() {
+        return "dashboard/patient";
     }
 
-    @GetMapping("/doctor/feedback")
-    public String viewFeedback() {
-        return "view_feedback";
+    @GetMapping("/patient/dashboard")
+    public String patientDashboard() {
+        return "dashboard/patient";
     }
 
-    @GetMapping("/patient/feedback/add")
-    public String addFeedback() {
-        return "add_feedback";
-    }
-
-    @GetMapping("/patient/doctor")
-    public String viewDoctor() {
-        return "view_doctors";
-    }
-
-    @GetMapping("/doctor/patient")
-    public String viewPatient() {
-        return "view_patient";
-    }
-
-    @GetMapping("/patient/appointment/add")
-    public String addAppointment() {
-        return "add_appointment";
-    }
-
-    @GetMapping("/patient/appointment/edit")
-    public String editAppointment() {
-        return "edit_appointment";
-    }
-
-    @GetMapping("/patient/appointment")
-    public String appointment() {
-        return "edit_appointment";
-    }
 }
