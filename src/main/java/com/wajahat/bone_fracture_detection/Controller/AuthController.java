6294aace93a5c1c6b0eba39277002d3dc362f58f
technicalwajahat <wajahat.awan.924@gmail.com>
2025-01-25 19:15:41 +0500

    get authenticated

--- src/main/java/com/wajahat/bone_fracture_detection/Controller/AuthController.java
+++ src/main/java/com/wajahat/bone_fracture_detection/Controller/AuthController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.core.Authentication;
 
 import com.wajahat.bone_fracture_detection.entity.Users;
 import com.wajahat.bone_fracture_detection.service.UserService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @Controller
@@ -22,14 +24,34 @@ public class AuthController {
     private UserService userService;
 
     @GetMapping("/register")
-    public String showRegistrationForm(Model model) {
+    public String showRegistrationForm(Model model, HttpServletRequest request, Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            if (authentication.getAuthorities().stream()
+                    .anyMatch(auth -> auth.getAuthority().equals("ROLE_PATIENT"))) {
+                return "redirect:/patient/";
+            } else if (authentication.getAuthorities().stream()
+                    .anyMatch(auth -> auth.getAuthority().equals("ROLE_DOCTOR"))) {
+                return "redirect:/doctor/";
+            }
+        }
+
         model.addAttribute("user", new Users());
         model.addAttribute("title", "Register");
         return "auth/register";
     }
 
     @GetMapping("/login")
-    public String showLoginForm(Model model) {
+    public String showLoginForm(Model model, HttpServletRequest request, Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            if (authentication.getAuthorities().stream()
+                    .anyMatch(auth -> auth.getAuthority().equals("ROLE_PATIENT"))) {
+                return "redirect:/patient/";
+            } else if (authentication.getAuthorities().stream()
+                    .anyMatch(auth -> auth.getAuthority().equals("ROLE_DOCTOR"))) {
+                return "redirect:/doctor/";
+            }
+        }
+
         model.addAttribute("title", "Login");
         return "auth/login";
     }
